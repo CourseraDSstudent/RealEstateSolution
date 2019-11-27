e0cbb3fef0505d3688e5d66d73131049f7197ed1
GitHAECstudent1 <nikosxanos@gmail.com>
2019-11-27 20:21:23 +0200

    just a comment to test the remote update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -35,5 +35,7 @@ public class Main {
         House rentedHouse = spitogatos.rentHouse(2333, 12, 12);
         System.out.println("I rented my house from spitogatos: " + rentedHouse.toString());
 
+        // a github test
+
     }
 }
